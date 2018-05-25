28bb86df1f6e4ae297503f3b04886375a951889d
Chenweihua <xjbrhnhh@163.com>
2018-05-25 23:14:15 +0800

    django-3-czbk-begin

---
+++ django/test_py2_dj182/test4/booktest/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from models import *
+
+
+def index(request):
+    # hero=HeroInfo.objects.get(pk=1)
+    # context={'hero': hero}
+
+    list=HeroInfo.objects.filter(isDelete=False)
+    context={'list': list}
+    return render(request, 'booktest/index.html', context)
+
+def show(request, id):
+    context={'id': id}
+    return render(request, 'booktest/show.html', context)
